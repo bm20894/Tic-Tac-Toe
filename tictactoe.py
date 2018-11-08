53457a9d18ba3bb0fa2c4a7a2be211c297a02d84
bm20894 <41974447+bm20894@users.noreply.github.com>
2018-11-08 11:26:47 -0600

    Added tic_tac_toe.py

---
+++ tictactoe.py
@@ -0,0 +1,84 @@
+'''
+Program: Tic Tac Toe Game
+Two players play tic tac toe together: X and O players
+A collaborative project made by 6th block APCSP
+
+7 teams
+Elements of the game:
+
+	1) User Input: Get input from player X and player O: store player marker
+		(X or O) and where they are playing on the board in variables.
+
+	2) Place Move in Board: Place either an X or an O (depending on who's
+		turn it is) where the user wants to place it.
+
+	Check For Win Cases (3 types):
+
+		3) Horizontal: If any player has all of their markers in the same row,
+			They win!
+
+		4) Vertical: If any player has all markers in the same index of all rows,
+			They win!
+
+		5) Diagonal: If any player has all markers in ascending or descending indecies
+			in all rows, They win!
+
+	6) Display Current Game Board: Print the game board to show the user what
+		it looks like with their markers in the board
+
+	7) Keep playing: loop until someone wins!
+
+'''
+
+''' -------------------USER INPUT----------------------- '''
+
+
+
+''' ---------------------------------------------------- '''
+
+
+
+''' ---------------PLACE MOVES IN BOARD----------------- '''
+
+
+
+''' ---------------------------------------------------- '''
+
+
+
+''' ---------------HORIZONTAL WIN CASE------------------ '''
+
+
+
+''' ---------------------------------------------------- '''
+
+
+
+''' ----------------VERTICAL WIN CASE------------------- '''
+
+
+''' ---------------------------------------------------- '''
+
+
+
+''' -----------------DIAGONAL WIN CASE------------------ '''
+
+
+
+''' ---------------------------------------------------- '''
+
+
+
+''' -----------------DISPLAY GAME BOARD----------------- '''
+
+
+
+''' ---------------------------------------------------- '''
+
+
+
+''' ------------------LOOP UNTIL WIN-------------------- '''
+
+
+
+''' ---------------------------------------------------- '''
