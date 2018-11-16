5926ce1c32399767c8121671e8ee7119682346e9
bi29338 <bi29338@students.mcpasd.k12.wi.us>
2018-11-16 10:54:16 -0600

    I just added something

--- tictactoe.py
+++ tictactoe.py
@@ -52,7 +52,36 @@ A collaborative project made by 6th block APCSP
 
 
 ''' ------------------LOOP-UNTIL-WIN-------------------- '''
+repeat = True
+while(repeat == True):
+    current_player = 0
+    while(i<10):
+        game_board = initializeboard()
+        current_player = "X"
+        player1 = user_input()
+        a = Checkhorizontal()
+        b = Checkvertical()
+        c = Checkdiagnal()
+        if(a == True or b == True or c == True):
+            print("player 1 won")
+            break
+        game_board = initializeboard()
+        current_player = "O"
+        player2 = user_input()
+        a = Checkhorizontal()
+        b = Checkvertical()
+        c = Checkdiagnal()
+        if(a == True or b == True or c == True):
+            print("player 2 won")
+            break
+    final = str(input("enter 'yes' to play again"))
+    if(final != 'yes'):
+        repeat = False
 
+        
+        
+
+    
 
 
 ''' ---------------------------------------------------- '''
