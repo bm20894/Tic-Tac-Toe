e8d9b5730e41dfcdfc8149a57329eb2f31d5a66a
kc23737 <kc23737@students.mcpasd.k12.wi.us>
2018-11-16 10:54:27 -0600

    i just added something!

--- tictactoe.py
+++ tictactoe.py
@@ -37,6 +37,18 @@ A collaborative project made by 6th block APCSP
 
 ''' -----------------DIAGONAL-WIN-CASE------------------ '''
 
+def check_diagonal():
+    global game_board
+    #Initialize win to false
+    win = False
+    #Check to see if the middle is ocupied by any player. If so checks to see if the corners match, if not returns win as false.
+    if game_board[1][1] != "":
+        if game_board[0][0] == game_board[1][1] and game_board[1][1] == game_board[2][2]:    #Checks upper left to lower right diagonal.
+            win = True        #Changes win to true
+        elif game_board[0][2] == game_board[1][1] and game_board[1][1] == game_board[2][0]:    #Checks upper right to lower left diagonal.
+            win = True    #Changes win to true
+    return win        #Returns win
+
 
 
 ''' ---------------------------------------------------- '''
